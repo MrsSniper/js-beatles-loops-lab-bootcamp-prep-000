6920900f6364234e043fb89fbaaa91d0bec92a09
Nessa S <bnvsnipe@gmail.com>
2018-04-05 16:08:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesArray = []
+  for(var i=0;  i < musicians.length; i++) {
+     beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesArray
+}
+
+function johnLennonFacts(facts) {
+  var loudFacts = [];
+  var counter = 0;
+  while(counter < facts.length) {
+    loudFacts.push(facts[counter] + "!!!");
+    counter++;
+  }
+  return loudFacts;
+}
+
+function iLoveTheBeatles(n) {
+  
+}
